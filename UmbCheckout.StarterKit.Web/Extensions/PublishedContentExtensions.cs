0a51a43ab93572878cd589ff590661a379a00ab2
Aaron <aaron@umbhost.net>
2023-08-11 15:25:21 +0100

    Started added bootstrap layouts

---
+++ UmbCheckout.StarterKit.Web/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace UmbCheckout.StarterKit.Web.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static IPublishedContent? GetHomePage(this IPublishedContent content) => content.Root();
+
+        public static IPublishedContent? GetSiteSettings(this IPublishedContent content) =>
+            GetHomePage(content)?.FirstChildOfType("siteSettings");
+    }
+}
