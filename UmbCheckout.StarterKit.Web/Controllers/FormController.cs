ba90cb6182e83660d9f1276a9b3125e2a2827c42
Aaron <aaron@umbhost.net>
2023-08-18 10:49:57 +0100

    Working Form Controller

--- UmbCheckout.StarterKit.Web/Controllers/FormController.cs
+++ UmbCheckout.StarterKit.Web/Controllers/FormController.cs
@@ -1,29 +1,39 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Mail;
 using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.Email;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Web.Website.Controllers;
-using Umbraco.Community.Contentment.DataEditors;
 
 namespace UmbCheckout.StarterKit.Web.Controllers
 {
     public class FormController : SurfaceController
     {
-        public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider)
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger<FormController> _logger;
+        private readonly GlobalSettings _globalSettings;
+        public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, IEmailSender emailSender, ILogger<FormController> logger, IOptions<GlobalSettings> globalSettings)
             : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
         {
+            _emailSender = emailSender;
+            _logger = logger;
+            _globalSettings = globalSettings.Value;
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult SubmitForm()
+        public async Task<IActionResult> SubmitForm()
         {
             var formCollection = HttpContext.Request.Form;
 
@@ -39,40 +49,50 @@ namespace UmbCheckout.StarterKit.Web.Controllers
 
                 if (form != null)
                 {
-                    foreach (var formFieldItem in form.Content.Value<IEnumerable<BlockListItem>>("formFields"))
-                    {
-                        foreach (var formField in formCollection)
-                        {
-                            if (Guid.TryParse(formField.Key, out var fieldKey) && formFieldItem.Content.Key == fieldKey)
-                            {
-                                if (formFieldItem.Content.Value<bool>("required") &&
-                                    string.IsNullOrEmpty(formField.Value))
-                                {
-                                    ModelState.AddModelError(string.Empty, formFieldItem.Content.Value<string>("validationErrorMessage"));
-                                }
-
-                                if (formFieldItem.Content.Value<bool>("validation"))
-                                {
-                                    if (formFieldItem.Content.Value<string>("validationType") == "customValidation" &&
-                                        !string.IsNullOrEmpty(
-                                            formFieldItem.Content.Value<string>("customValidationRegex")) &&
-                                        !Regex.IsMatch(formField.Value, formFieldItem.Content.Value<string>("customValidationRegex")))
-                                    {
-                                        ModelState.AddModelError(string.Empty, formFieldItem.Content.Value<string>("validationErrorMessage"));
-                                    }
-                                    else if (!Regex.IsMatch(formField.Value,
-                                                 formFieldItem.Content.Value<string>("validationType")))
-                                    {
-                                        ModelState.AddModelError(string.Empty, formFieldItem.Content.Value<string>("validationErrorMessage"));
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    var formFields = form.Content.Value<IEnumerable<BlockListItem>>("formFields");
+                    var formMessageField = form.Content.Value<IEnumerable<BlockListItem>>("message");
+                    ValidateForm(formFields, formCollection);
+                    ValidateForm(formMessageField, formCollection, true);
 
                     if (ModelState.IsValid)
                     {
-                        return RedirectToUmbracoPage(form.Content.Value<IPublishedContent>("confirmationPage"));
+                        try
+                        {
+                            var messageBlock = formMessageField?.FirstOrDefault();
+                            var messageKey = messageBlock.Content.Key.ToString("N");
+                            var formMessage = formCollection[messageKey];
+                            var fromAddress = form.Content.HasValue("fromEmailAddress") ? form.Content.Value<string>("fromEmailAddress") : _globalSettings.Smtp.From;
+
+                            var subject = form.Content.Value<string>("subject");
+
+                            var sb = new StringBuilder();
+                            sb.AppendLine(form.Content.Value<string>("messageIntroduction"));
+                            sb.AppendLine();
+
+                            foreach (var formField in formCollection.Where(x => Guid.TryParse(x.Key, out _)))
+                            {
+                                if (formFields.Select(x => x.Content.Key).Any(x => x == Guid.Parse(formField.Key)))
+                                {
+                                    var label = formFields.FirstOrDefault(x => x.Content.Key == Guid.Parse(formField.Key))
+                                        .Content.Value<string>("label");
+
+                                    sb.AppendLine($"{label}: {formField.Value}");
+                                }
+                            }
+
+                            sb.AppendLine($"{messageBlock.Content.Value<string>("label")}: {formMessage}");
+
+                            EmailMessage message = new EmailMessage(fromAddress, form.Content.Value<string>("toEmailAddress"), subject, sb.ToString(), false);
+                            await _emailSender.SendAsync(message, emailType: "Form Submission");
+                            TempData["formSubmittedSuccessfully"] = true;
+
+                            return RedirectToUmbracoPage(form.Content.Value<IPublishedContent>("confirmationPage"));
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, "Error When Submitting Contact Form");
+                            ModelState.AddModelError(string.Empty, "An error occurred trying to submit the form");
+                        }
                     }
                 }
 
@@ -84,6 +104,40 @@ namespace UmbCheckout.StarterKit.Web.Controllers
             }
 
             return CurrentUmbracoPage();
+        }
+
+        private void ValidateForm(IEnumerable<BlockListItem> formFields, IFormCollection formCollection, bool alwaysRequired = false)
+        {
+            foreach (var formFieldItem in formFields)
+            {
+                foreach (var formField in formCollection)
+                {
+                    if (Guid.TryParse(formField.Key, out var fieldKey) && formFieldItem.Content.Key == fieldKey)
+                    {
+                        if (!alwaysRequired && formFieldItem.Content.Value<bool>("required") &&
+                            string.IsNullOrEmpty(formField.Value))
+                        {
+                            ModelState.AddModelError(string.Empty, formFieldItem.Content.Value<string>("validationErrorMessage"));
+                        }
+
+                        if (formFieldItem.Content.Value<bool>("validation"))
+                        {
+                            if (formFieldItem.Content.Value<string>("validationType") == "customValidation" &&
+                                !string.IsNullOrEmpty(
+                                    formFieldItem.Content.Value<string>("customValidationRegex")) &&
+                                !Regex.IsMatch(formField.Value, formFieldItem.Content.Value<string>("customValidationRegex")))
+                            {
+                                ModelState.AddModelError(string.Empty, formFieldItem.Content.Value<string>("validationErrorMessage"));
+                            }
+                            else if (!Regex.IsMatch(formField.Value,
+                                         formFieldItem.Content.Value<string>("validationType")))
+                            {
+                                ModelState.AddModelError(string.Empty, formFieldItem.Content.Value<string>("validationErrorMessage"));
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
